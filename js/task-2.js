99052ff635782239a47d7063833637ef903da7aa
Yuliia1991 <155849406+Yuliia1991@users.noreply.github.com>
2024-05-22 19:07:54 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (message <= maxLength) {
+    if (message.Length <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
